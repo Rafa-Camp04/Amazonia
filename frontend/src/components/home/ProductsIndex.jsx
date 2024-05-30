522cfe224ee01a0497f86ecc0eba86785251c7cd
Rafael Campos <rafaelcampos04@hotmail.com>
2024-05-30 13:44:29 -0400

    styling home page

--- frontend/src/components/home/ProductsIndex.jsx
+++ frontend/src/components/home/ProductsIndex.jsx
@@ -2,6 +2,13 @@ import './ProductsIndex.css';
 
 function ProductsIndex() {
 
+
+    return(
+        <div className='product-index-body'>
+            <div></div>
+        </div>
+    )
+
 }
 
 export default ProductsIndex;
