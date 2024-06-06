410fa676fd0f387f7de0b5c603b3c5b906b2ebd8
Rafa-Camp04 <rafaelcampos04@hotmail.com>
2024-06-06 16:59:38 -0400

    styling delivery page

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,5 @@ Rails.application.initialize!
 
 Jbuilder.key_format camelize: :lower
 Jbuilder.deep_format_keys true
+
+# 3a5f919b022defb0ba3de3cffb48376 old master key
--- frontend/src/components/cart/Delivery.jsx
+++ frontend/src/components/cart/Delivery.jsx
@@ -3,23 +3,65 @@ import Navigation from '../navigation/Navigation';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import * as cartItemsActions from '../../store/cart';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Delivery() {
     const dispatch = useDispatch();
     const allCartItems = useSelector(state => state.cartItems)
+    const [initialCartItems, setInitialCartItems] = useState({});
 
     useEffect(() => {
+
+        setInitialCartItems(allCartItems);
+
         Object.values(allCartItems).forEach(item => {
             dispatch(cartItemsActions.deleteCartItem(item));
         });
     }, [dispatch, allCartItems]);
 
+    const itemsQuantity = () => {
+        let cartQuantity = 0;
+
+        Object.values(initialCartItems).map(item => {
+            cartQuantity += item.quantity
+        })
+
+        return cartQuantity
+    };
+
+
     return(
         <>
             <Navigation />
             <div id='delivery-body'>
-                
+                <div id='main-delivery-div'>
+
+                    <div id='order-delivery-message-block'>
+                        <h2 id='order-delivery-message-header'>Order placed, thanks!</h2>
+                        <p id='order-delivery-confirmation-paragraph'>Confirmation will be sent to your email.</p>
+                        <p id='order-quantity-paragraph'>{itemsQuantity()} items will be shipped to you by Amazonia.com.</p>
+
+                        <div className='division-between-sections'></div>
+                        
+                        <div id='bottom-message-block'>
+                            <div>
+                                <p id='order-delivery-bold-paragraph-1'>Order Number:</p>
+                                <p id='order-delivery-bold-paragraph-2'>Guaranteed delivery: <span id='delivery-date'>Jun. 04, 3024</span></p>
+                            </div>
+
+                            <Link to={'/'} id='order-delivery-home-button'>
+                                <input id='add-to-cart-button' type='submit' value={'Home'} />
+                            </Link>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div id='order-delivery-links-block'>
+
+                </div>
+
             </div>
         </>
     )
