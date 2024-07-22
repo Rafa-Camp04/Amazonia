4025c4ed7c5c86023b22da2a000b14a0685cfb16
Rafa-Camp04 <rafaelcampos04@hotmail.com>
2024-07-21 23:24:39 -0400

    update login form

--- frontend/src/components/session/LoginForm.jsx
+++ frontend/src/components/session/LoginForm.jsx
@@ -30,7 +30,7 @@ function LoginForm() {
       .catch(async (res) => {
         let data;
         try {
-          // .clone() essentially allows you to read the response body twice
+          /// .clone() essentially allows you to read the response body twice
           data = await res.clone().json();
         } catch {
           data = await res.text(); // Will hit this case if the server is down
